7febb6d4520e5e4125ce81c87f163b26520cd3b0
Patricia Solar <patricia.solar@yahoo.com>
2026-03-14 00:37:20 +0000

    modified requirements again
    just to ensure environment stability; some packages phased out some stuff mid project </3

--- install.bat
+++ install.bat
@@ -2,5 +2,5 @@
 echo Installing Python dependencies for Vocal FX Controller...
 pip install -r requirements.txt
 echo.
-echo Please ensure loopMIDI is installed and a port named 'Gesture Port' is created.
+echo Please ensure loopMIDI is installed and a port named 'FLGesture' is created.
 pause
